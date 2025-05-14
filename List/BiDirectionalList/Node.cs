fd47487b74edcbf990122ae9cf882dabe274e9a7
flistrr <flistrrr@gmail.com>
2025-05-14 10:09:04 +0300

    fixed

---
+++ List/BiDirectionalList/Node.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace BiDirectionalList;
+
+public class Node
+{
+    private Node? next;
+    private Node? prev;
+    private double data;
+
+    public double Data {
+        get => data;
+        set => data = value;
+    }
+    public Node? Next {
+        get => next;
+        set => next = value;
+    }
+    public Node? Prev {
+        get => prev;
+        set => prev = value;
+    }
+
+    public Node(double data)
+    {
+        this.data = data;
+        next = null;
+        prev = null;
+    }
+}
